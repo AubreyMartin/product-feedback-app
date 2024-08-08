741d6acb6c3feefdf82a7cac644405ba4a3d5002
AubreyMartin <aubreymartin9000@gmail.com>
2024-08-08 19:36:28 +0530

    feature/JSE-41-Button-Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,38 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './shared-library/theme';
-import { Button, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>
-        <Typography variant="h1" component="h2" gutterBottom>
-          Welcome to Material-UI
-        </Typography>
+       <Stack  direction={'row' } gap={2} >
         <Button variant="contained" color="primary">
-          Hello World
+          Button 1
         </Button>
-      </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Welcome to Aubrey Martin's project</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+        <Button variant="contained" color="info">
+          Button 2
+        </Button>
+
+        <Button variant="contained" color="secondary">
+          Button 3
+        </Button>
+
+        <Button variant="contained" color="error">
+          Button 4
+        </Button>
+
+    
+    
+        </Stack>
+      
     </ThemeProvider>
   );
 }
 
 export default App;
-
-
-// // src/App.tsx
-// import React from 'react';
-// import { Button, Typography } from '@mui/material';
-
-// const App: React.FC = () => {
-//   return (
-//     <div>
-//       <Typography variant="h1" color="primary">
-//         Hello, MUI!
-//       </Typography>
-//       <Button variant="contained" color="primary">
-//         Primary Button
-//       </Button>
-//       <Button variant="contained" color="secondary">
-//         Secondary Button
-//       </Button>
-//     </div>
-//   );
-// };
-
-// export default App;
