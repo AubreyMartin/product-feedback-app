3641bd58da835e8c72d6ee15ed1f8cfca43dd5a1
AubreyMartin <aubreymartin9000@gmail.com>
2024-08-12 14:29:40 +0530

    JSE-40-mui-setup Set up JOST font and typography styles in theme.ts
    - Installed JOST font.
    - Configured typography settings for H1, H2, H3, H4, Body 1, Body 2, and Body 3 as per design system.
    - Added necessary font weights and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import './App.css';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './shared-library/theme';
-import Button from '@mui/material/Button';
-import { Stack } from '@mui/material';
+import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./shared-library/theme";
+import Button from "@mui/material/Button";
+import { Stack } from "@mui/material";
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-       <Stack  direction={'row' } gap={2} >
+      <Stack direction={"row"} gap={2}>
         <Button variant="contained" color="primary">
           Button 1
         </Button>
-    
+
         <Button variant="contained" color="info">
           Button 2
         </Button>
@@ -26,11 +25,7 @@ function App() {
         <Button variant="contained" color="error">
           Button 4
         </Button>
-
-    
-    
-        </Stack>
-      
+      </Stack>
     </ThemeProvider>
   );
 }
--- src/shared-library/theme.ts
+++ src/shared-library/theme.ts
@@ -1,81 +1,107 @@
 // src/theme/theme.ts
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-    secondary: {
-      main: '#dc004e',
-    },
-  },
   typography: {
+    fontFamily: "Jost, sans-serif",
     h1: {
-      fontSize: '2rem',
+      fontWeight: 700, // Jost Bold
+      fontSize: "24px",
+      lineHeight: "35px",
+      letterSpacing: "-0.33px",
+    },
+    h2: {
+      fontWeight: 700, // Jost Bold
+      fontSize: "20px",
+      lineHeight: "29px",
+      letterSpacing: "-0.25px",
+    },
+    h3: {
+      fontWeight: 700, // Jost Bold
+      fontSize: "18px",
+      lineHeight: "26px",
+      letterSpacing: "-0.25px",
+    },
+    h4: {
+      fontWeight: 700, // Jost Bold
+      fontSize: "14px",
+      lineHeight: "20px",
+      letterSpacing: "-0.2px",
+    },
+    body1: {
+      fontWeight: 400, // Jost Regular font
+      fontSize: "16px",
+      lineHeight: "24px",
+      letterSpacing: "normal",
+    },
+    body2: {
+      fontWeight: 400, // Jost Regular font
+      fontSize: "14px",
+      lineHeight: "22px",
+      letterSpacing: "normal",
+    },
+     body3: {
+      fontWeight: 600, // Jost Semibold
+      fontSize: '13px',
+      lineHeight: '19px',
+      letterSpacing: 'normal',
     },
   },
+
   components: {
     MuiButton: {
-      styleOverrides:{
+      styleOverrides: {
         root: {
-         borderRadius:8,
-         padding:'8px 16px',
-         textTransform: 'none',
+          borderRadius: 8,
+          padding: "8px 16px",
+          textTransform: "none",
         },
       },
-
 
       variants: [
         {
-          props: { variant: 'contained', color: 'primary' },
+          props: { variant: "contained", color: "primary" },
           style: {
-              backgroundColor: '#A03FEA',
-              color: '#FFFFFF',
-              '&:hover': {
-                  backgroundColor: '#B967F1',
-              },
-          },
-      },
-
-      {
-        props: { variant: 'contained', color: 'info' },
-        style: {
-            backgroundColor: '#4661E6', // look at the figma for the exact colors
-            color: '#FFFFFF', // FFF since, our text is white
-            '&:hover': {
-                backgroundColor: '#7C91F9', // look at the figma for the exact colors
+            backgroundColor: "#A03FEA",
+            color: "#FFFFFF",
+            "&:hover": {
+              backgroundColor: "#B967F1",
             },
-        },
-    },
-
-    {
-      props: { variant: 'contained', color: 'secondary' },
-      style: {
-          backgroundColor: '#3A4374',
-          color: '#FFFFFF',
-          '&:hover': {
-              backgroundColor: '#656EA3',
           },
-      },
-  },
-
-    {
-    props: { variant: 'contained', color: 'error' },
-    style: {
-        backgroundColor: '#D73737',
-        color: '#FFFFFF',
-        '&:hover': {
-            backgroundColor: '#E98888',
         },
-    },
-    },
 
-    
+        {
+          props: { variant: "contained", color: "info" },
+          style: {
+            backgroundColor: "#4661E6", // look at the figma for the exact colors
+            color: "#FFFFFF", // FFF since, our text is white
+            "&:hover": {
+              backgroundColor: "#7C91F9", // look at the figma for the exact colors
+            },
+          },
+        },
 
+        {
+          props: { variant: "contained", color: "secondary" },
+          style: {
+            backgroundColor: "#3A4374",
+            color: "#FFFFFF",
+            "&:hover": {
+              backgroundColor: "#656EA3",
+            },
+          },
+        },
 
-
-
+        {
+          props: { variant: "contained", color: "error" },
+          style: {
+            backgroundColor: "#D73737",
+            color: "#FFFFFF",
+            "&:hover": {
+              backgroundColor: "#E98888",
+            },
+          },
+        },
       ],
     },
   },
