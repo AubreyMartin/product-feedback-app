7b6ca272e39943fdc462ff3140b649fbd88bcc9e
AubreyMartin <aubreymartin9000@gmail.com>
2024-08-21 17:08:12 +0530

    new files

---
+++ src/shared-library/Arrow/ArrowDown.tsx
@@ -0,0 +1,14 @@
+const ArrowDown  = () => {
+
+    return (
+       
+        <svg width="11" height="7" viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.33398 1L5.33398 5L9.33398 1" stroke="#4661E6" stroke-width="2"/>
+      </svg>
+      
+
+         );
+   }
+
+
+ export default ArrowDown;
